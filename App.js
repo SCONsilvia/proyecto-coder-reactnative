581dca2e83c1481fc57463cc6c54088835014af5
SCON <silvianohemy38@gmail.com>
2026-05-27 06:58:44 -0300

    agregando persistencia al modo oscuro o claro

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
-import { store } from "./src/store/store";
+import { store, persistor } from "./src/store/store";
+import { PersistGate } from "redux-persist/integration/react";
 import AppContent from "./AppContent";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <Provider store = {store}>
-        <AppContent />
+        <PersistGate loading = {null} persistor = {persistor}>
+          <AppContent />
+        </PersistGate>
       </Provider>
     </SafeAreaProvider>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import userReducer from "../features/auth/userSlice";
 import drawingsReducer from "../features/drawings/drawingsSlice";
 import themeReducer from "../features/theme/themeSlice";
 
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        drawings: drawingsReducer,
-        theme: themeReducer,
-    },
+const persistConfig = {
+    key: "root",
+    storage: AsyncStorage,
+    whitelist: ["theme"], // solo persiste el tema, no user ni drawings
+};
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    drawings: drawingsReducer,
+    theme: themeReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH, 
+                    REHYDRATE,
+                    PAUSE, 
+                    PERSIST, 
+                    PURGE, 
+                    REGISTER],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
 
 
